0408f178a1ee506aa80bf792862a9f27a44ce86f
esteban-q-diaz <estebandpal@gmail.com>
2021-04-03 01:04:50 -0700

    React enviornment has been set up

---
+++ client/components/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import ReactDOM from 'react-dom'
+
+function app() {
+  return (
+    <div>
+      <h1>React APP is working</h1>
+    </div>
+  )
+}
+
+export default app
