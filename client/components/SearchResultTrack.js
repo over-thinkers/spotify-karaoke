5747cd91c58f469af0350e93c6256585c6805756
Jeremy Fujimoto <jeremyfujimoto@gmail.com>
2021-04-08 09:08:48 -1000

    Add selectTrack function

--- client/components/SearchResultTrack.js
+++ client/components/SearchResultTrack.js
@@ -1,9 +1,9 @@
 import React from 'react'
 
-const SearchResultTrack = ({ track }) => {
+const SearchResultTrack = ({ track, selectTrack }) => {
   const { artist, title, uri, albumUrl } = track;
   return (
-    <div>
+    <div onClick={() => selectTrack(uri)} >
       <img src={albumUrl} />
       <div>
         <div>{title}</div>
