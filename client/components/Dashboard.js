0ef4a63e9d2410efc274f1c9d99839041bcf52ea
Jeremy Fujimoto <jeremyfujimoto@gmail.com>
2021-04-08 09:07:28 -1000

    Add getMe so we can use getUserPlaylists

--- client/components/Dashboard.js
+++ client/components/Dashboard.js
@@ -2,9 +2,11 @@ import React, { Fragment, useState, useEffect } from 'react'
 import useAuth from './useAuth'
 import SpotifyWebApi from 'spotify-web-api-node';
 import SearchResultTrack from './SearchResultTrack';
+import AudioPlayer from './AudioPlayer';
 
 const spotifyApi = new SpotifyWebApi({
-  clientId: 'fe60e323d09f45e7bf069353680f5a1a'
+  // clientId: 'fe60e323d09f45e7bf069353680f5a1a'
+  clientId: 'dca3db4a5a914cae9632a6c5ebba47f0'
 })
 
 const Dashboard = ({ code }) => {
@@ -12,6 +14,12 @@ const Dashboard = ({ code }) => {
   const [search, setSearch] = useState('');
   const [searchResults, setSearchResults] = useState([]);
   const [delay, setDelay] = useState();
+
+  const [tracks, setTracks] = useState([]);
+
+  const selectTrack = (uri) => {
+    setTracks([...tracks, uri])
+  }
 
   const searchTracks = () => {
     spotifyApi.searchTracks(search)
@@ -36,6 +44,16 @@ const Dashboard = ({ code }) => {
   useEffect(() => {
     if (accessToken) {
       spotifyApi.setAccessToken(accessToken);
+
+      spotifyApi.getMe()
+      .then(data => {
+        console.log(data)
+        spotifyApi.getUserPlaylists(data.body.id)
+          .then(res => console.log(res))
+      })
+      .catch(err => {
+        console.log(err)
+      })
     }
   }, [accessToken])
 
@@ -61,9 +79,10 @@ const Dashboard = ({ code }) => {
       />
       <div>
         {searchResults.map(track => (
-          <SearchResultTrack track={track} key={track.uri} />
+          <SearchResultTrack track={track} key={track.uri} selectTrack={selectTrack} />
         ))}
       </div>
+      <AudioPlayer accessToken={accessToken} tracks={tracks} />
     </>
   )
 }
