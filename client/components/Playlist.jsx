9795dff6743700a50249310b5a0d2888fcda08e2
esteban-q-diaz <Esteban@Estebans-MacBook-Pro.local>
2021-04-09 18:40:27 -0700

    added song playlist queue box

--- client/components/Playlist.jsx
+++ client/components/Playlist.jsx
@@ -4,15 +4,21 @@ import axios from 'axios'
 import PlaylistItems from './PlaylistItems'
 
 function Playlist() {
-    const [playlist, setPlaylist] = useState([])
-    const [user, setUser] = useState('')
+    const [playlist, setPlaylist] = useState([
+        {artist: "Gryffin", title: "Feel Good (feat. Daya)", albumUrl: "https://i.scdn.co/image/ab67616d0000b2736362397987db1fbf17f3d9b5"},
+        {artist: "Gryffin", title: "I Want Love (with Two Feet)", albumUrl: "https://i.scdn.co/image/ab67616d0000b27327942224c75fa4f0718f1b19"},
+        {artist: "Gryffin", title: "Body Back (feat. Maia Wright)", albumUrl: "https://i.scdn.co/image/ab67616d0000b273a2c3c702bdb4ad9ed5789fe5"},
+        {artist: "Gryffin", title: "All You Need To Know (feat. Calle Lehmann)", albumUrl: "https://i.scdn.co/image/ab67616d0000b27344fa3ab08e3c6e74c1960081"},
+        {artist: "Gryffin", title: "Body Back (feat. Maia Wright)", albumUrl: "https://i.scdn.co/image/ab67616d0000b273a2c3c702bdb4ad9ed5789fe5"},
+        {artist: "Gryffin", title: "All You Need To Know (feat. Calle Lehmann)", albumUrl: "https://i.scdn.co/image/ab67616d0000b27344fa3ab08e3c6e74c1960081"},
+    ])
+    // const [user, setUser] = useState('')
     let [currentSong, setCurrentSong] = useState([])
     let location = useLocation();
     currentSong = location.state
-    console.log(location.state)
+    // console.log("song data", location.state)
 
-    playlist.push(location.state)
-    // playlist = location.state
+    // playlist.push(location.state)
 
     useEffect(() => {
         const artist = location.state.artist;
@@ -49,9 +55,15 @@ function Playlist() {
   return (
     <>
         <div className="playlistContainer">
+            <div className="playlistHeader"><h1 className="playlistH1">My Playlist</h1></div>
+            <div className="playlistBox">
+                {/* <h1 className="playlistH1">My Playlist</h1> */}
            {playlist.map((songs, index) => {
+            //    <div className="playlistSongList">
                return <PlaylistItems songs={songs} key={index}/>
+            //    </div>
            })}
+           </div>
         </div>
     </>
   )
--- client/components/PlaylistItems.jsx
+++ client/components/PlaylistItems.jsx
@@ -3,10 +3,19 @@ import React from 'react';
 function PlaylistItems({ songs }) {
   return (
     <>
-        <div>
-            {songs.artist}
-            {songs.title}
-            <img src={songs.albumUrl}/>
+        <div className="playlistSongList">
+
+          <div className="playlistTitle">
+            {`+ ${songs.title} by ` }
+          </div>
+
+          <div className="playlistAritst">
+            {` ${songs.artist} `}
+          </div>
+
+          <div className="playlistAlubum">
+            <img className="playlistImg" src={songs.albumUrl}/>
+          </div>
         </div>
     </>
   )
