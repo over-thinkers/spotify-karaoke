217557289e7b566e43f0a85f8ed56eb47d24ef52
Jeremy Fujimoto <jeremyfujimoto@gmail.com>
2021-04-12 14:00:52 -1000

    Try useRef

--- client/components/AudioPlayer.js
+++ client/components/AudioPlayer.js
@@ -1,16 +1,24 @@
-import React, { useEffect } from 'react'
+import React, { useEffect, useRef } from 'react'
 import SpotifyPlayer from 'react-spotify-web-playback'
 
 function AudioPlayer({ accessToken, tracks }) {
   if (!accessToken) return null;
 
+  const playerRef = useRef();
+
+  console.log('ref', playerRef)
+
+  playerRef.current.props.uris.push()
+
+
   return (
     <SpotifyPlayer
+      ref={playerRef}
       token={accessToken}
       showSaveIcon
       uris={tracks}
       callback={state => {
-        console.log(state);
+        console.log('hello', state);
       }}
     />
   )
