bc0f4b603022cd360d9f85ffcfcc8727899ec788
Jeremy Fujimoto <jeremyfujimoto@gmail.com>
2021-04-14 17:38:51 -1000

    Player auto plays when current song changes

--- client/components/AudioPlayer.js
+++ client/components/AudioPlayer.js
@@ -1,14 +1,19 @@
-import React, { useEffect, useRef, useContext } from 'react'
+import React, { useState, useEffect, useRef, useContext } from 'react'
 import SpotifyPlayer from 'react-spotify-web-playback'
 import SongContext from '../../context/SongContext'
 
 function AudioPlayer({ accessToken }) {
   if (!accessToken) return null;
+
+  const [play, setPlay] = useState(false);
   const context = useContext(SongContext);
+
+  useEffect(() => {
+    setPlay(true);
+  }, [context.currentSong])
 
   return (
     <div 
-      className="playerContainer" 
       style={{
         width: '100%',
         position: 'fixed',
@@ -16,14 +21,12 @@ function AudioPlayer({ accessToken }) {
       }}
     >
       <SpotifyPlayer
+        play={play}
         token={accessToken}
         showSaveIcon
         uris={context.currentSong}
         callback={state => {
-          console.log('Player state changed: ', state);
-          if (state.type === 'player_update' && state.position === 0) {
-            console.log('Load the next song now')
-          }
+          if (!state.isPlaying) setPlay(false)
         }}
       />
     </div>
