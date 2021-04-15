90298c6271e431f726c2547cbec36c9577ede076
Jeremy Fujimoto <jeremyfujimoto@gmail.com>
2021-04-14 20:01:56 -1000

    Context now keeps track of a playlist, current track, and idx. Also change song functions

--- context/SongContext.js
+++ context/SongContext.js
@@ -1,13 +1,40 @@
-import React, { createContext, useState } from 'react';
+import React, { createContext, useState, useEffect } from 'react';
 
 const SongContext = createContext();
 
 export const SongContextProvider = (props) => {
-  const [currentSong, setCurrentSong] = useState([]);
+  const [currentSong, setCurrentSong] = useState(null);
+  const [playlist, setPlaylist] = useState([]);
+  const [playlistIdx, setPlaylistIdx] = useState(0);
 
+  const addToPlaylist = (track) => {
+    if (!currentSong) {
+      setCurrentSong(track);
+    } 
+    setPlaylist(prev => [...prev, track]);
+  }
+
+  const nextSong = () => {
+    if (!playlist[playlistIdx + 1]) return;
+    setPlaylistIdx(prev => prev + 1);
+  }
+  
+  const prevSong = () => {
+    if (!playlist[playlistIdx - 1]) return;
+    setPlaylistIdx(prev => prev - 1);
+  }
+  
+  useEffect(() => {
+    if (!playlist.length) return;
+    setCurrentSong(playlist[playlistIdx]);
+  }, [playlistIdx])
+  
   const context = {
     currentSong,
-    setCurrentSong
+    setCurrentSong,
+    addToPlaylist,
+    nextSong,
+    prevSong
   };
 
   return (
