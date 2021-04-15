72cd8bd263623574588946b3a410ec7e2078c83e
Jeremy Fujimoto <jeremyfujimoto@gmail.com>
2021-04-14 15:34:45 -1000

    Remove unneeded fragments

--- client/components/NavLoggedIn.jsx
+++ client/components/NavLoggedIn.jsx
@@ -5,51 +5,49 @@ import { Link } from 'react-router-dom';
 
 function NavLoggedIn() {
   return (
-    <>
-      <nav>
-        <ul className="nav-list2">
-          <li className="nav-item2">
-            <div className="logoName">
-              <div className="logo">
-                <a href="#"><img className="logoIcon" src="https://ez-drum-kit.s3-us-west-1.amazonaws.com/chat.png" width="42px"/></a>
-              </div>
-              <div className="name">
-                <p>Spoti-oki</p>
-              </div>
+    <nav>
+      <ul className="nav-list2">
+        <li className="nav-item2">
+          <div className="logoName">
+            <div className="logo">
+              <a href="#"><img className="logoIcon" src="https://ez-drum-kit.s3-us-west-1.amazonaws.com/chat.png" width="42px"/></a>
             </div>
-          </li>
-          <li className="nav-item2">
-          <Link to={{ pathname: '/'}}>
-            <a href="#">Home</a>
-          </Link>
-          </li>
-          <li className="nav-item2">
-            <a href="#">About</a>
-          </li>
-          <li className="nav-item2">
-            <a href="#">My Playlist</a>
-          </li>
-          <li className="nav-item2">
-            <p className="line"></p>
-          </li>
-          <li className="nav-item2">
-            <a href="#" onClick={()=>{alert('modal to logout')}}>
-                <div className='loggedIn'>
-                    <div className="userLogo">
-                        <p><FaRegUserCircle size={30}/></p>
-                    </div>
-                    <div className='profile'>
-                        <p>Profile</p>
-                    </div>
-                    <div className='arrow'>
-                        <p><BsChevronDown size={18}/></p>
-                    </div>
-                </div>
-            </a>
-          </li>
-        </ul>
-      </nav>
-    </>
+            <div className="name">
+              <p>Spoti-oki</p>
+            </div>
+          </div>
+        </li>
+        <li className="nav-item2">
+        <Link to={{ pathname: '/'}}>
+          <a href="#">Home</a>
+        </Link>
+        </li>
+        <li className="nav-item2">
+          <a href="#">About</a>
+        </li>
+        <li className="nav-item2">
+          <a href="#">My Playlist</a>
+        </li>
+        <li className="nav-item2">
+          <p className="line"></p>
+        </li>
+        <li className="nav-item2">
+          <a href="#" onClick={()=>{alert('modal to logout')}}>
+              <div className='loggedIn'>
+                  <div className="userLogo">
+                      <p><FaRegUserCircle size={30}/></p>
+                  </div>
+                  <div className='profile'>
+                      <p>Profile</p>
+                  </div>
+                  <div className='arrow'>
+                      <p><BsChevronDown size={18}/></p>
+                  </div>
+              </div>
+          </a>
+        </li>
+      </ul>
+    </nav>
   )
 }
 
