6de6fd653ff1fb69f1d7c59c688dfa742478f522
Jeremy Fujimoto <jeremyfujimoto@gmail.com>
2021-04-16 09:27:04 -1000

    Prettier

--- context/SongContext.js
+++ context/SongContext.js
@@ -12,97 +12,97 @@ export const SongContextProvider = (props) => {
   const addToPlaylist = (track) => {
     if (!currentSong) {
       setCurrentSong(playlist[playlistIdx]);
-    } 
-    setPlaylist(prev => [...prev, track]);
-  }
+    }
+    setPlaylist((prev) => [...prev, track]);
+  };
 
   const nextSong = () => {
     if (!playlist[playlistIdx + 1]) return;
-    setPlaylistIdx(prev => prev + 1);
-  }
-  
+    setPlaylistIdx((prev) => prev + 1);
+  };
+
   const prevSong = () => {
     if (!playlist[playlistIdx - 1]) return;
-    setPlaylistIdx(prev => prev - 1);
-  }
+    setPlaylistIdx((prev) => prev - 1);
+  };
 
   const deleteSong = (index) => {
-    setPlaylist(prev => (
-      prev.filter((track, i) => (
-        i !== index
-      ))
-    ))
+    setPlaylist((prev) => prev.filter((track, i) => i !== index));
     if (index < playlistIdx) {
-      setPlaylistIdx(prev => prev - 1);
+      setPlaylistIdx((prev) => prev - 1);
     }
-  }
-  
+  };
+
   useEffect(() => {
     if (!playlist.length) return;
     if (currentSong.uri === playlist[playlistIdx].uri) return;
     setCurrentSong(playlist[playlistIdx]);
-  }, [playlistIdx])
-  
-  useEffect(() => {
-    if (!userEmail) return;
-
-    axios.get('http://localhost:3000/user', {
-      params: { email: userEmail }
-    })
-    .then(res => {
-      console.log('GOT USER', res)
-      setPlaylist(res.data[0].playlist);
-      // if it's a new user
-      if (!res.data.length) {
-        axios.post('http://localhost:3000/user', {
-          email: userEmail
-        })
-        .then(res => {
-          console.log('CREATED USER', res)
-        })
-        .catch(err => {
-          console.log('Error creating user', err);
-        })
-      }
-    })
-    .catch(err => {
-      console.log('Error getting user', err)
-    })
-  }, [userEmail])
+  }, [playlistIdx]);
 
   useEffect(() => {
     if (!userEmail) return;
 
-    axios.put('http://localhost:3000/playlist', {
-      email: userEmail,
-      playlist
-    })
-    .then(res => {
-      console.log('UPDATED PLAYLIST', res);
-    })
-    .catch(err => {
-      console.log('Error updating playlist', err);
-    })
-  }, [playlist])
+    axios
+      .get('http://localhost:3000/user', {
+        params: { email: userEmail },
+      })
+      .then((res) => {
+        console.log('GOT USER', res);
+        setPlaylist(res.data[0].playlist);
+        // if it's a new user
+        if (!res.data.length) {
+          axios
+            .post('http://localhost:3000/user', {
+              email: userEmail,
+            })
+            .then((res) => {
+              console.log('CREATED USER', res);
+            })
+            .catch((err) => {
+              console.log('Error creating user', err);
+            });
+        }
+      })
+      .catch((err) => {
+        console.log('Error getting user', err);
+      });
+  }, [userEmail]);
+
+  useEffect(() => {
+    if (!userEmail) return;
+
+    axios
+      .put('http://localhost:3000/playlist', {
+        email: userEmail,
+        playlist,
+      })
+      .then((res) => {
+        console.log('UPDATED PLAYLIST', res);
+      })
+      .catch((err) => {
+        console.log('Error updating playlist', err);
+      });
+  }, [playlist]);
 
   const context = {
     currentSong,
     playlist,
     setCurrentSong,
+    playlistIdx,
     setPlaylistIdx,
     userEmail,
     setUserEmail,
     addToPlaylist,
     nextSong,
     prevSong,
-    deleteSong
+    deleteSong,
   };
 
   return (
     <SongContext.Provider value={context}>
       {props.children}
     </SongContext.Provider>
-  )
-}
+  );
+};
 
 export default SongContext;
