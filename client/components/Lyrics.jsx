f4ab20f227994472dc849f7574a26c2d58fdd63c
Jeremy Fujimoto <jeremyfujimoto@gmail.com>
2021-04-16 09:25:23 -1000

    Clean up unused stuff

--- client/components/Lyrics.jsx
+++ client/components/Lyrics.jsx
@@ -1,42 +1,40 @@
 import React, { useState, useEffect, useContext } from 'react';
-import { useLocation} from "react-router";
+import { useLocation } from 'react-router';
 import axios from 'axios';
 import SongContext from '../../context/SongContext';
 
-
 function Lyrics() {
-    // let [currentSong, setCurrentSong] = useState([])
-    const context = useContext(SongContext);
-    // let location = useLocation();
-    // currentSong = location.state
-    const [lyrics, setLyrics] = useState('')
+  const context = useContext(SongContext);
+  const [lyrics, setLyrics] = useState('');
 
-    useEffect(() => {
-        if (!context.currentSong) return;
-        axios.get('http://localhost:3000/lyrics', {
-            params: {
-                track: context.currentSong.title,
-                artist: context.currentSong.artist,
-            }
-        })
-        .then((res) => {
-            setLyrics(res.data.lyrics)
-        })
-    }, [context.currentSong])
+  useEffect(() => {
+    if (!context.currentSong) return;
+
+    axios
+      .get('http://localhost:3000/lyrics', {
+        params: {
+          track: context.currentSong.title,
+          artist: context.currentSong.artist,
+        },
+      })
+      .then((res) => {
+        setLyrics(res.data.lyrics);
+      });
+  }, [context.currentSong]);
 
   return (
-    <div className="lyricContainer">
-        <div className="lyricTitle">
-            <h1>{context.currentSong.title} by</h1>
-        </div>
-        <div className="lyricArtist">
-            <h1>{context.currentSong.artist}</h1>
-        </div>
-        <div className="lyricLyrics">
-            <p>{lyrics}</p>
-        </div>
+    <div className='lyricContainer'>
+      <div className='lyricTitle'>
+        <h1>{context.currentSong.title} by</h1>
+      </div>
+      <div className='lyricArtist'>
+        <h1>{context.currentSong.artist}</h1>
+      </div>
+      <div className='lyricLyrics'>
+        <p>{lyrics}</p>
+      </div>
     </div>
-  )
+  );
 }
 
-export default Lyrics
+export default Lyrics;
