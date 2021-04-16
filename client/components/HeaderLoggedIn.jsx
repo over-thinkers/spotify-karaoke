1bc164d5e5c5c5b4c9cf823ad5c42912a35cb15a
Jeremy Fujimoto <jeremyfujimoto@gmail.com>
2021-04-16 12:02:52 -1000

    Playlist button functional

--- client/components/HeaderLoggedIn.jsx
+++ client/components/HeaderLoggedIn.jsx
@@ -1,19 +1,19 @@
 import React, { useState } from 'react';
+import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom';
 
 function HeaderLoggedIn() {
-
   return (
-    <>
-      <div className="headerContainer2">
-            <div className="title2">
-                <h1>Sing Aloud. Be Happy</h1>
-            </div>
-            <div className="playlistButton2">
-                <button className='button2' onClick={()=>{alert('send user to Playlist page')}}>MY PLAYLIST</button>
-            </div>
+    <div className='headerContainer2'>
+      <div className='title2'>
+        <h1>Sing Aloud. Be Happy</h1>
       </div>
-    </>
-  )
+      <div className='playlistButton2'>
+        <Link to='/playlist'>
+          <button className='button2'>MY PLAYLIST</button>
+        </Link>
+      </div>
+    </div>
+  );
 }
 
-export default HeaderLoggedIn
+export default HeaderLoggedIn;
