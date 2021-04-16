434e6e144e33ceb5a400eda3e926fd7a8184737e
Jeremy Fujimoto <jeremyfujimoto@gmail.com>
2021-04-16 09:27:43 -1000

    Clean up transition to Mongo

--- server/index.js
+++ server/index.js
@@ -4,112 +4,74 @@ const SpotifyWebApi = require('spotify-web-api-node');
 const path = require('path');
 const cors = require('cors');
 const bodyParser = require('body-parser');
-const lyricFinder = require('lyrics-finder')
+const lyricFinder = require('lyrics-finder');
 const { getUser, createUser, updatePlaylist } = require('../database/helpers');
 
 const app = express();
 const PORT = 3000;
 
-app.use(express.static(path.join(__dirname, '../public')))
+app.use(express.static(path.join(__dirname, '../public')));
 app.use(cors());
-app.use(bodyParser.json())
+app.use(bodyParser.json());
 
 app.post('/refresh', (req, res) => {
-  const refreshToken = req.body.refreshToken
+  const refreshToken = req.body.refreshToken;
   const spotifyApi = new SpotifyWebApi({
     redirectUri: process.env.REDIRECT_URI,
     clientId: process.env.CLIENT_ID,
     clientSecret: process.env.CLIENT_SECRET,
-    refreshToken
-  })
+    refreshToken,
+  });
 
   // clientId, clientSecret and refreshToken has been set on the api object previous to this call.
-spotifyApi.refreshAccessToken()
-  .then((data) => {
-    console.log('The access token has been refreshed!', data.body);
+  spotifyApi
+    .refreshAccessToken()
+    .then((data) => {
+      console.log('The access token has been refreshed!', data.body);
 
-    // Save the access token so that it's used in future calls
-    spotifyApi.setAccessToken(data.body['access_token']);
-  })
-  .catch(() => {
-    res.sendStatus(404)
-  })
-})
+      // Save the access token so that it's used in future calls
+      spotifyApi.setAccessToken(data.body['access_token']);
+    })
+    .catch(() => {
+      res.sendStatus(404);
+    });
+});
 
 app.post('/login', (req, res) => {
-    // passing the code from client side
-  const code = req.body.code
+  // passing the code from client side
+  const code = req.body.code;
 
   // need a token to access the Spotify API
   const spotifyApi = new SpotifyWebApi({
     redirectUri: process.env.REDIRECT_URI,
     clientId: process.env.CLIENT_ID,
-    clientSecret: process.env.CLIENT_SECRET
-  })
+    clientSecret: process.env.CLIENT_SECRET,
+  });
   // AUTHORIZE THAT WE HAVE A CODE
-  spotifyApi.authorizationCodeGrant(code)
+  spotifyApi
+    .authorizationCodeGrant(code)
     // Below is a promise that returns the
     // access token, refesh token and the expires in time
     // This is what the API returns
-    .then(data => {
-      console.log('server login data:', data)
+    .then((data) => {
+      console.log('server login data:', data);
       res.json({
         accessToken: data.body.access_token,
         refreshToken: data.body.refresh_token,
-        expiresIn: data.body.expires_in
-      })
+        expiresIn: data.body.expires_in,
+      });
     })
-    .catch(err => {
-      console.log('error at server login', err)
-      res.sendStatus(400)
-    })
-})
+    .catch((err) => {
+      console.log('error at server login', err);
+      res.sendStatus(400);
+    });
+});
 
 app.get('/lyrics', async (req, res) => {
-  const lyrics = await lyricFinder(req.query.artist, req.query.track) || 'No lyrics found'
-  res.json({lyrics})
-})
-
-app.get('/getplaylist', (req, res) => {
-  const userEmail = req.query.userEmail
-    db.getPlaylist(userEmail, (err, data) => {
-      if (err) {
-        res.status(404).send(err)
-      } else {
-        res.status(202).send(data)
-      }
-    })
-})
-
-app.post('/postsong', (req, res) => {
-  const artist = req.body.artist
-  const title = req.body.title
-  const albumUrl = req.body.albumUrl
-  const userEmail = req.body.userEmail
-
-    db.postSong({userEmail, artist, title, albumUrl}, (err, data) => {
-      if (err) {
-        res.status(404).send(err)
-      } else {
-        res.status(202).send(data)
-      }
-    })
-
-})
-
-app.put('/removesong', (req, res) => {
-  const songTitle = req.body.songTitle
-  const userEmail = req.body.userEmail
-  console.log("song to be removed:", songTitle, userEmail)
-
-  db.removeSong(songTitle, userEmail, (err, data) => {
-    if (err) {
-      res.status(404).send(err)
-    } else {
-      res.status(202).send(data)
-    }
-  })
-})
+  const lyrics =
+    (await lyricFinder(req.query.artist, req.query.track)) || 'No lyrics found';
+  res.json({ lyrics });
+});
 
 app.get('/user', (req, res) => {
   const { email } = req.query;
@@ -118,8 +80,8 @@ app.get('/user', (req, res) => {
       res.status(404).send(err);
     }
     res.status(200).send(data);
-  })
-})
+  });
+});
 
 app.post('/user', (req, res) => {
   const { email } = req.body;
@@ -129,8 +91,8 @@ app.post('/user', (req, res) => {
       res.status(400).send(err);
     }
     res.status(201).send(data);
-  })
-})
+  });
+});
 
 app.put('/playlist', (req, res) => {
   const { email, playlist } = req.body;
@@ -139,9 +101,9 @@ app.put('/playlist', (req, res) => {
       res.status(404).send(err);
     }
     res.status(200).send(data);
-  })
-})
+  });
+});
 
 app.listen(PORT, () => {
-    console.log(`Listening on port ${PORT}`)
-})
+  console.log(`Listening on port ${PORT}`);
+});
