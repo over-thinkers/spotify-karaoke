c5c61dd43ef52deb7d4200f81e08e503e17db44d
Jeremy Fujimoto <jeremyfujimoto@gmail.com>
2021-04-18 18:24:55 -1000

    Drag and drop working

--- client/components/Playlist.jsx
+++ client/components/Playlist.jsx
@@ -1,4 +1,5 @@
 import React, { useState, useEffect, useContext } from 'react';
+import { DragDropContext, Droppable, Draggable } from 'react-beautiful-dnd';
 import { useLocation } from 'react-router';
 import axios from 'axios';
 import PlaylistItem from './PlaylistItem';
@@ -8,22 +9,45 @@ function Playlist() {
   const context = useContext(SongContext);
   const playlist = context.playlist;
 
+  function handleOnDragEnd(result) {
+    const songs = [...playlist];
+    const [draggedItem] = songs.splice(result.source.index, 1);
+    songs.splice(result.destination.index, 0, draggedItem);
+
+    context.setPlaylist(songs);
+  }
+
   return (
     <div className='playlistContainer'>
       <div className='playlistHeader'>
         <h1 className='playlistH1'>My Playlist</h1>
       </div>
-      <div className='playlistBox'>
-        {playlist.map((song, index) => {
-          return (
-            <PlaylistItem
-              song={song}
-              key={song.uri}
-              index={index}
-            />
-          );
-        })}
-      </div>
+      <DragDropContext onDragEnd={handleOnDragEnd}>
+        <Droppable droppableId='songs'>
+          {(provided) => (
+            <ul
+              className='playlistBox'
+              {...provided.droppableProps}
+              ref={provided.innerRef}
+            >
+              {playlist.map((song, index) => (
+                <Draggable key={song.uri} draggableId={song.uri} index={index}>
+                  {(provided) => (
+                    <li
+                      {...provided.draggableProps}
+                      {...provided.dragHandleProps}
+                      ref={provided.innerRef}
+                    >
+                      <PlaylistItem song={song} index={index} />
+                    </li>
+                  )}
+                </Draggable>
+              ))}
+              {provided.placeholder}
+            </ul>
+          )}
+        </Droppable>
+      </DragDropContext>
     </div>
   );
 }
