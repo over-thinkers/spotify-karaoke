dc936edc793bf775478e4c2a2557dc193c7aa519
Jeremy Fujimoto <jeremyfujimoto@gmail.com>
2021-04-24 16:26:44 -1000

    Clean up code, transition to Emotion

--- client/components/Header.jsx
+++ client/components/Header.jsx
@@ -1,26 +1,30 @@
 import React, { useState } from 'react';
 import Login from './Login';
 import Dashboard from './Dashboard';
+import styled from '@emotion/styled';
+
+const Container = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  height: 100vh;
+  background-color: #2941ab;
+  color: #fff;
+`;
+
+const Title = styled.h1`
+  font-size: 5rem;
+  font-weight: bold;
+`;
 
 function Header() {
-    const code = new URLSearchParams(window.location.search).get('code');
-    const AUTH_URL = 'https://accounts.spotify.com/authorize?client_id=dca3db4a5a914cae9632a6c5ebba47f0&response_type=code&redirect_uri=http://localhost:3000/&scope=streaming%20user-read-email%20user-read-private%20user-library-read%20user-library-modify%20user-read-playback-state%20user-modify-playback-state'
-
-
   return (
-    <>
-        <div className="header-container">
-            <div className="title">
-                <h1>Log In. Using Spotify</h1>
-            </div>
-            <div className="playlistButton">
-                <a href={AUTH_URL}>
-                    <button className='button'>{code ? <Dashboard code={code} /> : <Login />}</button>
-                </a>
-            </div>
-      </div>
-    </>
-  )
+    <Container>
+      <Title>Log In. Using Spotify</Title>
+      <Login />
+    </Container>
+  );
 }
 
-export default Header
+export default Header;
