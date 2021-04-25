c062750d31d11e2c77a3f629af6e1c703b14fa66
Jeremy Fujimoto <jeremyfujimoto@gmail.com>
2021-04-24 16:27:11 -1000

    Clean up code, transition to Emotion

--- client/components/Login.js
+++ client/components/Login.js
@@ -1,9 +1,33 @@
 import React from 'react';
+import styled from '@emotion/styled';
 
-const AUTH_URL = 'https://accounts.spotify.com/authorize?client_id=dca3db4a5a914cae9632a6c5ebba47f0&response_type=code&redirect_uri=http://localhost:3000/&scope=streaming%20user-read-email%20user-read-private%20user-library-read%20user-library-modify%20user-read-playback-state%20user-modify-playback-state%20playlist-modify-public%20playlist-read-private'
+const AUTH_URL =
+  'https://accounts.spotify.com/authorize?client_id=dca3db4a5a914cae9632a6c5ebba47f0&response_type=code&redirect_uri=http://localhost:3000/&scope=streaming%20user-read-email%20user-read-private%20user-library-read%20user-library-modify%20user-read-playback-state%20user-modify-playback-state%20playlist-modify-public%20playlist-read-private';
+
+const Anchor = styled.a`
+  text-align: center;
+  margin-top: -30px;
+`;
+
+const LoginButton = styled.button`
+  border: 1px #31b954 solid;
+  border-radius: 50px;
+  height: 50px;
+  width: 250px;
+  background-color: #31b954;
+  color: #fff;
+  font-size: 17px;
+  font-weight: 500;
+  text-decoration: none;
+  &:hover {
+    cursor: pointer;
+  }
+`;
 
 export default function Login() {
   return (
-      <a href={AUTH_URL} className='button'>LOGIN</a>
-  )
+    <Anchor href={AUTH_URL}>
+      <LoginButton>LOGIN</LoginButton>
+    </Anchor>
+  );
 }
