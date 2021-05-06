e219dc56eaac828a02000794942584851aabc865
Jeremy Fujimoto <jeremyfujimoto@gmail.com>
2021-05-05 16:10:46 -1000

    MONGO_URI for Atlas stored in env

--- database/index.js
+++ database/index.js
@@ -1,25 +1,30 @@
 const mongoose = require('mongoose');
-mongoose.connect('mongodb://localhost/spotify-karaoke', {useNewUrlParser: true, useUnifiedTopology: true});
+mongoose.connect(process.env.MONGO_URI, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
 
 const db = mongoose.connection;
 db.on('error', console.error.bind(console, 'connection error:'));
-db.once('open', function() {
-  console.log('Connected to MongoDB!')
+db.once('open', function () {
+  console.log('Connected to MongoDB!');
 });
 
 const userSchema = new mongoose.Schema({
   email: String,
-  playlist: [{
-    artist: String,
-    title: String,
-    uri: String,
-    albumUrl: String,
-    length: Number,
-  }]
-})
+  playlist: [
+    {
+      artist: String,
+      title: String,
+      uri: String,
+      albumUrl: String,
+      length: Number,
+    },
+  ],
+});
 
 const User = mongoose.model('User', userSchema);
 
 module.exports = {
   User,
-}
+};
