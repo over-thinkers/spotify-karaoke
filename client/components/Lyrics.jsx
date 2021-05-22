4caddf85d70003412e24236264dcca63f02e08a1
Jeremy Fujimoto <jeremyfujimoto@gmail.com>
2021-05-21 19:22:21 -1000

    Fix bug where you couldn't dnd while auto scrolling

--- client/components/Lyrics.jsx
+++ client/components/Lyrics.jsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect, useContext } from 'react';
+import React, { useState, useEffect, useContext, useRef } from 'react';
 import { useLocation } from 'react-router';
 import axios from 'axios';
 import AppContext from '../../context/AppContext';
@@ -9,11 +9,18 @@ const LyricsContainer = styled.div`
   flex-direction: column;
   justify-content: flex-start;
   align-items: center;
-  background: rgb(158,0,255);
-  background: linear-gradient(114deg, rgba(158,0,255,1) 0%, rgba(244,1,138,1) 63%, rgba(250,167,91,1) 86%, rgba(254,61,20,1) 100%);
+  background: rgb(158, 0, 255);
+  background: linear-gradient(
+    114deg,
+    rgba(158, 0, 255, 1) 0%,
+    rgba(244, 1, 138, 1) 63%,
+    rgba(250, 167, 91, 1) 86%,
+    rgba(254, 61, 20, 1) 100%
+  );
   color: ${(props) => props.theme.colors.lyricsText};
-  min-height: 100vh;
+  height: 100vh;
   margin-left: 19rem;
+  overflow: scroll;
 `;
 
 const Title = styled.h2`
@@ -26,6 +33,7 @@ const SongLyrics = styled.p`
   line-height: 1.6;
   white-space: pre;
   font-size: 2vw;
+  margin-bottom: 10rem;
 `;
 
 function Lyrics() {
@@ -33,6 +41,7 @@ function Lyrics() {
   const [lyrics, setLyrics] = useState('');
   const [scrollInterval, setScrollInterval] = useState(null);
   const [level, setLevel] = useState(0);
+  const lyricsRef = useRef();
 
   useEffect(() => {
     clearInterval(scrollInterval);
@@ -41,7 +50,7 @@ function Lyrics() {
 
     setScrollInterval(
       setInterval(() => {
-        window.scrollBy({
+        lyricsRef.current.scrollBy({
           top: 1,
           behavior: 'smooth',
         });
@@ -61,7 +70,7 @@ function Lyrics() {
     setLyrics('');
 
     axios
-      .get('https://spotioke.herokuapp.com/lyrics', {
+      .get('http://localhost:3000/lyrics', {
         cancelToken: source.token,
         params: {
           track: context.currentSong.title,
@@ -91,14 +100,13 @@ function Lyrics() {
   if (!context.currentSong) return null;
 
   return (
-    <LyricsContainer>
+    <LyricsContainer ref={lyricsRef}>
       <Title>
         {context.currentSong.title}
         <br />
         by {context.currentSong.artist}
       </Title>
       <SongLyrics>{lyrics}</SongLyrics>
-
 
       <div className='mouse_scroll'>
         <div
